7da206f2ca6405ff06eb074185ab76cd4ccc5331
김혜진 <aa981204@naver.com>
2022-09-20 21:13:02 +0900

    rename Solution6.java

--- String_문자열/Solution6.java
+++ String_문자열/Solution6.java
@@ -1,15 +1,12 @@
 package String_문자열;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  *6. 중복 문자 제거
  * 소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램 작성
  */
-public class Solutuon6 {
+public class Solution6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
